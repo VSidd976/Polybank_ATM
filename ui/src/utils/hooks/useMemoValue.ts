4193d2522aba189cb13ddf7802e96d76daa3c578
vkyl <viktor.kyliukh@gmail.com>
2025-10-18 10:35:47 +0300

    Update useMemoValue.ts

--- ui/src/utils/hooks/useMemoValue.ts
+++ ui/src/utils/hooks/useMemoValue.ts
@@ -1,6 +1,6 @@
-import { useMemo } from "react";
+import { useMemo, type DependencyList } from "react";
 
-export const useMemoValue = <Args extends unknown[], Return>(
+export const useMemoValue = <Args extends DependencyList, Return>(
   callback: (...args: Args) => Return,
   args: Args,
 ): Return => {
