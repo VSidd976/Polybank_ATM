fa1d4fa6699c64b7e41f61c32a4e2fb36c1e4431
VSidd976 <siddv06@gmail.com>
2025-11-27 20:43:17 +0200

    lto

---
+++ cmake/LTO.cmake
@@ -0,0 +1,11 @@
+function(target_enable_lto TARGET)
+    include(CheckIPOSupported)
+    check_ipo_supported(RESULT result OUTPUT output)
+
+    if(result)
+        message(STATUS "IPO/LTO supported")
+        set_property(TARGET ${TARGET} PROPERTY INTERPROCEDURAL_OPTIMIZATION ON)
+    else()
+        message(WARNING "IPO/LTO not supported")
+    endif()
+endfunction(target_enable_lto)
